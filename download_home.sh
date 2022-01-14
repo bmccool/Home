61f0ccf1db7861dd4733abc53023b8f27e01acb8
Brendon McCool <BMccool@logikos.com>
2022-01-14 13:20:59 -0500

    Fix file ownership after initial download

--- download_home.sh
+++ download_home.sh
@@ -8,3 +8,5 @@ git remote add origin https://github.com/bmccool/home.git
 git fetch --all
 git reset --hard origin/master
 
+# Fix new/git files ownership
+chown -R brendon:brendon .vim .bash_profile .bashrc .screenrc .tmux.conf .vimrc README.md download_home.sh .git
