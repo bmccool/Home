578bbba7443ebbbb09e002240afef885e308bd4c
Brendon McCool <brendonmccool@gmail.com>
2024-02-07 20:24:56 -0500

    Update download_home.sh

--- download_home.sh
+++ download_home.sh
@@ -10,6 +10,23 @@ git remote add origin https://github.com/bmccool/home.git
 git fetch --all
 git reset --hard origin/master
 
+# Install Font
+mkdir ~/.fonts
+sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/ComicShannsMono.zip -O ~/.fonts/ComicShannsMono.zip
+sudo chmod u+rw ~/.fonts/*
+fc-cache -fv
+
+## Install Oh my Posh
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+
+## Download the themes
+mkdir ~/.poshthemes
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
+chmod u+rw ~/.poshthemes/*.json
+rm ~/.poshthemes/themes.zip
+
 # Fix new/git files ownership
 USER=$(stat -c '%U' .)
 chown -R $USER .vim .bash_profile .bashrc .screenrc .tmux.conf .vimrc README.md download_home.sh .git
